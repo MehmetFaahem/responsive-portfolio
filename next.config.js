bfa21b8c5e17322b96da8e4f0ea20c9e53cc386d
Mehmet Faahem <mehmetfaahem@gmail.com>
2022-10-29 22:42:31 +0600

    first commit

--- next.config.js
+++ next.config.js
@@ -4,4 +4,10 @@ const nextConfig = {
   swcMinify: true,
 }
 
-module.exports = nextConfig
+
+module.exports = {
+  images: {
+    domains: ['i.ibb.co', 'cdn.pixabay.com'],
+  },
+  nextConfig
+}
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}", // Note the addition of the `app` directory.
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'fav': ['Poppins', 'sans-serif']
+      },
+      screens: {
+        'mobile': '280px',
+        'tablet': '640px',
+        'laptop': '1024px',
+        'desktop': '1280px',
+      }
+    },
+  },
+  plugins: [],
+}
+
